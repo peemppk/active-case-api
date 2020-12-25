b9ba1a033872be3e79dfd27e6951d9f161326735
naphattharawat <naphattharawat@gmail.com>
2020-12-26 00:41:35 +0700

    update

--- src/routes/login.ts
+++ src/routes/login.ts
@@ -63,20 +63,4 @@ router.post('/verify', async (req: Request, res: Response) => {
   }
 });
 
-router.get('/check-event', async (req: Request, res: Response) => {
-  let eventCode: string = req.body.eventCode;
-  let hospcode: string = req.decoded.hospcode;
-  let db = req.db;
-  try {
-    const rs: any = await loginModel.checkEvent(db, hospcode, eventCode);
-    if(rs.length){
-      res.send({ ok: true, rows: rs });
-    } else {
-      res.send({ ok: false });
-    }
-  } catch (error) {
-    console.log(error);
-    res.send({ ok: false, error: error.message, code: HttpStatus.INTERNAL_SERVER_ERROR });
-  }
-});
 export default router;
